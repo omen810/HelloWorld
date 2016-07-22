6518aec1392f2871589c4cc168cbbd83d582a2a9
omen810 <8561073@qq.com>
2016-07-22 08:57:34 +0800

    second

--- app/src/main/java/com/omen/www/helloworld/MainActivity.java
+++ app/src/main/java/com/omen/www/helloworld/MainActivity.java
@@ -1,5 +1,6 @@
 package com.omen.www.helloworld;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -8,23 +9,76 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
 public class MainActivity extends AppCompatActivity {
     private EditText activity_main_edt_username;
     private Button activity_main_btn_submit;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        activity_main_edt_username=(EditText)findViewById(R.id.edt_username);
-        activity_main_btn_submit=(Button)findViewById(R.id.btn_submit);
+        activity_main_edt_username = (EditText) findViewById(R.id.edt_username);
+        /*activity_main_btn_submit=(Button)findViewById(R.id.btn_submit);*/
+        activity_main_btn_submit = (Button) findViewById(R.id.btn_submit);
         activity_main_btn_submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.d("omen","you have clicked the submit button");
-                String username=activity_main_edt_username.getText().toString();
-                Toast.makeText(MainActivity.this,username,Toast.LENGTH_SHORT).show();
+                Log.d("omen", "you have clicked the submit button");
+                String username = activity_main_edt_username.getText().toString();
+                Toast.makeText(MainActivity.this, username, Toast.LENGTH_SHORT).show();
             }
         });
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.omen.www.helloworld/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.omen.www.helloworld/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
     }
 }
